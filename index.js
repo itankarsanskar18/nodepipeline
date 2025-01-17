859d3acfe5e089cc3c50d6d4b617ad16aac7960c
itankarsanskar <sanskaritankar123@gmail.com>
2025-01-17 11:43:57 +0000

    commit1

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// Respond with "changed not" for requests that hit the root "/"
+app.get('/', function (req, res) {
+  res.send('changed not');
+});
+
+// Listen to port 3000 by default
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server is running on port 3000');
+});
+
+module.exports = app;
